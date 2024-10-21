d115cdce48b012e3c53d782eaa2030f6262724e8
João Dalri <105470028+Dalri05@users.noreply.github.com>
2024-10-20 21:40:09 -0300

    feat : cria utils nullSafe

---
+++ AccountService/src/main/java/com/socialCloneMicroservices/AccountService/Utils/StringUtils.java
@@ -0,0 +1,8 @@
+package com.socialCloneMicroservices.AccountService.Utils;
+
+public class StringUtils {
+
+    public static String trim(String str) {
+        return str == null ? "null" : str.trim();
+    }
+}
